ba2319c7510739306e52356f53067b04939056fd
patrick.Z <775150@qq.com>
2023-01-03 19:41:55 +0800

    feat(get_loading): translation support

--- get_loading/lib/get_loading.dart
+++ get_loading/lib/get_loading.dart
@@ -1,7 +1,6 @@
 library get_loading;
 
-export 'package:get_loading/src/get_loading_extension.dart';
-export 'package:get_loading/src/get_loading_module.dart';
-
 export 'package:get_loading/src/event/get_loading_event.dart';
 export 'package:get_loading/src/event/get_loading_status_changed_event.dart';
+export 'package:get_loading/src/get_loading_extension.dart';
+export 'package:get_loading/src/get_loading_module.dart';
--- get_loading/lib/src/get_loading_module.dart
+++ get_loading/lib/src/get_loading_module.dart
@@ -1,12 +1,23 @@
-import 'dart:async';
-
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:get_module/get_module.dart';
 
+/// GetLoadingModule
+///
+/// register [EasyLoading] and translation
 class GetLoadingModule extends Module {
   @override
   void install() {
     Get.moduleConfig.builders.add(EasyLoading.init());
+    Get.appendTranslations({
+      'en': {
+        'get.loading.success': 'Success',
+        'get.loading.error': 'Failed',
+      },
+      'zh_CN': {
+        'get.loading.success': '成功',
+        'get.loading.error': '失败',
+      }
+    });
   }
 }
